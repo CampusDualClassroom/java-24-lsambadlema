ced176772887c999f3f726b01dca066f3e8e8cc5
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-28 12:42:54 +0100

    Terminado Exercise24

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,13 +5,27 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Smith");
+        queue.add("Montessori");
+        queue.add("Peralta");
+        queue.add("House");
+        return queue;
 
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
 
     }
 
     public static void main(String[] args) {
+
+        Queue<String> queue = createQueue();
+
+        System.out.println("Recorriendo y vaciando la cola");
+        printAndEmptyQueue(queue);
 
     }
 
